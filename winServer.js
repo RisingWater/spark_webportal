21884d3cdee38909e61b71f56f82153d20de972f
4680414@qq.com <4680414@qq.com>
2019-11-04 16:22:02 +0800

    修改winServer.js

--- winServer.js
+++ winServer.js
@@ -1,16 +1,13 @@
-var db = require('./src/backend/db/db.js');
-var server_config = db.load_server_config();
-
 let Service = require('node-windows').Service;
  
 let svc = new Service({
     name: 'WeixunServer Portal',
     description: 'WeixunServer简易接入管理系统',
-    script: server_config.server_dir + '/webserver.js'
+    script: require('path').join(__dirname, './src/backend/webserver.js')
 });
  
 svc.on('install', () => {
     svc.start();
 });
- 
+
 svc.install();
